07259ea544b58c86d99a00373c9d300f64debea9
Darshan <darshancsjjarrett@gmail.com>
2023-10-22 23:46:15 -0500

    Edited Java

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,87 +1,53 @@
-window.onload = function(){
-    const game=document.getElementById("game");
-    const status=document.getElementById("status");
-    const board=document.getElementById("board");
-    const controls=document.getElementsByClassName("controls");
-    const button=document.getElementsByClassName("btn")[0];
+document.addEventListener('DOMContentLoaded', function() {
+    const board = document.getElementById("board");
+    const squares = board.querySelectorAll('div');
+    const button = document.querySelector('.btn');
+    const status = document.getElementById("status");
+    const winCriteria = [
+        [0, 1, 2], [0, 3, 6], [0, 4, 8], [3, 4, 5], [6, 7, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6]
+    ]; 
+    
+    let currentPlayer = 'X'; 
 
-    const e= board.querySelectorAll('div');
-    let play = 'X';
-    let poslst=['', '', '', '', '', '', '', '', ''];
+    let positions = Array(9).fill(''); 
+    
+    // Set up the board
+    squares.forEach((square, index) => {
+        square.classList.add("square");
+        square.addEventListener('click', () => handleUserMove(square, index));
+        square.addEventListener('mouseover', () => square.classList.add('hover'));
+        square.addEventListener('mouseout', () => square.classList.remove('hover'));
+    }); // box changes color when mouse hovers
 
-    const wincrit= [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-
-    for (let z=0; z<=8; z++){
-        e[z].setAttribute("class", "square");
-    }
-
-    const user= (element,index) =>{
-        console.log(element.innerText)
-        if(element.innerText !== 'X' || element.innerText !== 'O'){
-            element.innerText= play;
-            element.classList.add(play);
-            poslst[index]=play;
-            console.log(poslst);
-            verifywin();
-            if(play === 'X'){
-                play='O';
-            }
-            else
-            {
-                play='X';
-            }
+    function handleUserMove(square, index) {
+        if (!positions[index]) {
+            square.innerText = currentPlayer;
+            square.classList.add(currentPlayer);
+            positions[index] = currentPlayer;
+            checkForWin();
+            currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
         }
+    } // Places 'X' or 'O' in the box depending on which player's turn it is
 
-    }
-    
-    e.forEach( (element, index) => {
-        element.addEventListener('click', () => user(element, index));
-        element.addEventListener('mouseover', function(){
-            element.classList.add('hover');
-        });
-        element.addEventListener('mouseout', function(){
-            element.classList.remove('hover');
-        });
-    });
-   
-    
-    button.addEventListener('click', ()=>{
-        poslst=['', '', '', '', '', '', '', '', ''];
-        status.innerHTML= 'Move your mouse over a square and click to play an X or an O.'
-        status.classList.remove('you-won');
-        e.forEach(element =>{
-            element.innerText ='';
-            element.classList.remove('X');
-            element.classList.remove('O');
-        });
-    });
-
-    function verifywin(){
-        for(let w=0; w<=7; w++){
-            const win = wincrit[w];
-    
-            const pos1= poslst[win[0]];
-            const pos2 = poslst[win[1]];
-            const pos3 = poslst[win[2]];
-            if (pos1 === ''|| pos2 === ''|| pos3===''){
-                continue;
-            }  
-            if (pos1=== pos2 && pos2 === pos3){
-                status.innerHTML= 'Congratulations! ' + pos1 +' is the winner';                
+    function checkForWin() {
+        for (const win of winCriteria) {
+            const [a, b, c] = win;
+            if (positions[a] && positions[a] === positions[b] && positions[a] === positions[c]) {
+                status.innerHTML = `Congratulations! ${positions[a]} is the winner`;
                 status.classList.add('you-won');
                 break;
-            }  
+            }
+        } 
+    } 
 
-        }
-    }
-}
+    button.addEventListener('click', () => {
+        positions.fill('');
+        status.innerHTML = 'Move your mouse over a square and click to play an X or an O.';
+        status.classList.remove('you-won');
+        squares.forEach(square => {
+            square.innerText = '';
+            square.className = 'square';
+        });
+        currentPlayer = 'X';
+    }); // starts a new game by clearing all the boxes
+});
